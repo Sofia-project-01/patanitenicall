0d718f95c3a805acd793f78799b8ec6330c76055
usmamlenu <usmamlenu@gmail.com>
2024-03-01 15:51:50 +0700

    delete : buttom edit

--- src/components/warehouse/index.tsx
+++ src/components/warehouse/index.tsx
@@ -7,8 +7,7 @@ const Warehouse: React.FC = () => {
   return (
     <div>
       <Row className='space-x-2 flex justify-end'>
-        <Link to="/AddProduce"> <Button className='bg-sky-500 w-[100px] pr-2 pl-2'>Add Produce</Button></Link>
-        <Link to="/EditPageTable"><Button className='bg-sky-500 w-[100px] pr-2 pl-2'>Edit Produce</Button></Link>
+        <Link to="/AddProduce"> <Button className='text-white bg-sky-600 w-[100px] pr-2 pl-2'>Add Produce</Button></Link>
       </Row>
       <div className='mt-4'>
         <TeblePage />
